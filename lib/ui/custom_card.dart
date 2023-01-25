59ea2b2e54106779f02307e7bae49a540e93edf6
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2023-01-25 16:44:05 +0600

    Update & Delete data

--- lib/ui/custom_card.dart
+++ lib/ui/custom_card.dart
@@ -10,23 +10,32 @@ class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    var snapshotData = snapshot.docs[index];
+
+    var docId = snapshotData.id;
     var timeToDate = DateTime.fromMillisecondsSinceEpoch(
-        snapshot.docs[index]['timestamp'].seconds *1000);
+        snapshotData['timestamp'].seconds *1000);
     var dateFormatted = DateFormat("MMM,EEE,d").format(timeToDate);
+    var collectionReference = FirebaseFirestore.instance.collection("boardappdatabasecollection#1");
+
+    TextEditingController nameInputController = TextEditingController(text: snapshotData['name']);
+    TextEditingController titleInputController = TextEditingController(text: snapshotData['title']);
+    TextEditingController descriptionInputController = TextEditingController(text: snapshotData['description']);
+
     return Column(
       children: [
         Container(
-          height: 130,
+          height: 170,
           child: Card(
             elevation: 15,
             child: Column(
               children: [
                 ListTile(
-                  title: Text(snapshot.docs[index]['title']),
-                  subtitle: Text(snapshot.docs[index]["description"].toString()),
+                  title: Text(snapshotData['title']),
+                  subtitle: Text(snapshotData["description"].toString()),
                   leading: CircleAvatar(
                     radius: 35,
-                    child: Text(snapshot.docs[index]['title'].toString()[0]),
+                    child: Text(snapshotData['title'].toString()[0]),
                   ),
                 ),
                 //for showing saving date & time
@@ -35,10 +44,94 @@ class CustomCard extends StatelessWidget {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Text("by: ${snapshot.docs[index]['name']}  "),
+                      Text("by: ${snapshotData['name']}  "),
                       Text((dateFormatted)),
                     ],
                   ),
+                ),
+                //Update & Delete data from firestore database
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(icon:Icon(Icons.edit,size: 20,),
+                      onPressed: () async{
+                      await showDialog(context: context,
+                          builder: (BuildContext context){
+                        return AlertDialog(
+                          contentPadding: EdgeInsets.all(10),
+                          content: Column(
+                            children: [
+                              Text("Please fill out the Form."),
+                              Expanded(
+                                  child: TextField(
+                                    autofocus: true,
+                                    autocorrect: true,
+                                    decoration: InputDecoration(
+                                        label: Text("Your Name*")
+                                    ),
+                                    controller: nameInputController,
+                                  )),
+                              Expanded(
+                                  child: TextField(
+                                    autofocus: true,
+                                    autocorrect: true,
+                                    decoration: InputDecoration(
+                                        label: Text("Title*")
+                                    ),
+                                    controller: titleInputController,
+                                  )),
+                              Expanded(
+                                  child: TextField(
+                                    autofocus: true,
+                                    autocorrect: true,
+                                    decoration: InputDecoration(
+                                        label: Text("description*")
+                                    ),
+                                    controller: descriptionInputController,
+                                  ))
+                            ],
+                          ),
+                          actions: [
+                            TextButton(onPressed:() {
+                              nameInputController.clear();
+                              titleInputController.clear();
+                              descriptionInputController.clear();
+
+                              Navigator.pop(context);
+                            },
+                                child:Text("Cancel")),
+
+                            //update data on firestore from app
+                            TextButton(onPressed: (){
+                              if(nameInputController.text.isNotEmpty &&
+                                  titleInputController.text.isNotEmpty &&
+                                  descriptionInputController.text.isNotEmpty){
+                                collectionReference.doc(docId).update({
+                                  "name" : nameInputController.text,
+                                  "title" : titleInputController.text,
+                                  "description" : descriptionInputController.text,
+                                  "timestamp" : DateTime.now()
+                                }).then((value) => Navigator.pop(context));
+                              }
+
+                            },
+                                child: Text("Update"))
+                          ],
+                        );
+                          });
+
+                    },
+                        ),
+                    SizedBox(height: 20),
+
+                    IconButton(
+                    icon:Icon(Icons.delete,size: 20,),
+                        onPressed: () async {
+                      await collectionReference
+                      .doc(docId).delete();
+                    },
+                        )
+                  ],
                 )
                 //snapshot.docs[index]['timestamp'].toString())
               ],
